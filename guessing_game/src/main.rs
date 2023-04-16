657703dc2664e4296d3943aa78c325621638ac10
new name <agam.swaroop619@gmail.com>
2023-04-16 20:15:26 +0530

    2.5 commit

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,13 +1,17 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
-    println!("Enter a String");
 
+    //Entering Input here
+    println!("Enter a String");
+    //Mutable variable
     let mut guess = String::new();
+    //Inputing the data
     io::stdin()
     .read_line(&mut guess)
     .expect("Failed to get String guess");
-
+    //To check half part of the code
     println!("The no. {} is what you entered",guess);
-       
+
 }
